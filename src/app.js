4136c7e0edee95be0b8ae8eb0aa9b5f5ac779211
FLYSASA <812071523@qq.com>
2020-08-04 23:55:53 +0800

    modify: 引入parcel，重构代码结构

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './button.vue'
+
+Vue.component('g-button', Button)
+
+new Vue({
+    el: '#app',
+})
---
+++ src/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="g-button"></button>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+<style lang="less">
+.g-button {
+    padding: 0 1em;
+}
+</style>
