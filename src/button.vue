505952cee0590670da618de2052faf69951f3b64
FLYSASA <812071523@qq.com>
2020-08-07 00:12:16 +0800

    feature: button组件新增icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,39 +1,65 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-    data() {
-      return {};
-    },
-
-    components: {},
-
-    computed: {},
-
-    mounted: {},
-
-    methods: {}
+    props: {
+      icon:{
+        type: String,
+        default: ''
+      },
+      iconPosition: {
+        type: String,
+        default: 'left'
+      }
+    }
   }
 </script>
 <style lang='less' scoped>
   .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
-    font: inherit;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover{
+    vertical-align: middle;
+    &:hover {
       border-color: var(--border-color-hover);
     }
-    &:active{
+    &:active {
       background-color: var(--button-active-bg);
     }
     &:focus {
       outline: none;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+    > .content{
+      order: 2;
+    }
+    &.icon-right{
+      > .icon{
+        order: 2;
+        margin-left: .3em;
+        margin-right: 0;
+      }
+      > .content {
+        order:1;
+      }
     }
   }
 </style>
