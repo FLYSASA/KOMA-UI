5513aa3ad916f68be0390ea1cebcc879b14f2feb
聂腾飞 <812071523@qq.com>
2020-08-13 09:25:37 +0800

    feat: 给button按钮新增loading动画

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <g-icon class="loading" name="loading"></g-icon>
     <g-icon  class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
@@ -25,6 +26,10 @@
   }
 </script>
 <style lang='less' scoped>
+  @keyframes spin {
+    0% { transform: rotate(0deg);}
+    100% { transform: rotate(360deg);}
+  }
   .g-button {
     display: inline-flex;
     justify-content: center;
@@ -36,19 +41,10 @@
     border: 1px solid var(--border-color);
     background: var(--button-bg);
     vertical-align: middle;
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-    > .icon {
-      order: 1;
-      margin-right: .3em;
-    }
+    &:hover {border-color: var(--border-color-hover);}
+    &:active { background-color: var(--button-active-bg); }
+    &:focus { outline: none; }
+    > .icon { order: 1; margin-right: .3em; }
     > .content{
       order: 2;
     }
@@ -61,6 +57,9 @@
       > .content {
         order:1;
       }
+    }
+    .loading {
+      animation: spin 1s infinite linear; // infinite 无限循环， linear 线性匀速
     }
   }
 </style>
