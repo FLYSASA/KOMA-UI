8c12f2c22924b34ec5ae324fcf4acc285453e28f
FLYSASA <812071523@qq.com>
2020-08-13 23:50:33 +0800

    feat: 新增button-group组件，处理极端情况

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {},
+        data() {
+            return {};
+        },
+        mounted() {
+            for (let node of this.$el.children) {
+                if (node.nodeName.toLowerCase() === 'div') {
+                    console.warn(`g-button-group 的子元素应该全是g-button, 但是你写的是 ${ node.nodeName }`)
+                }
+            }
+        }
+    }
+</script>
+<style lang='less' scoped>
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        >.g-button {
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+
+            &:hover {
+                z-index: 1;
+            }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+        }
+    }
+</style>
