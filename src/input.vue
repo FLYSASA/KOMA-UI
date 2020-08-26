f91f501e05626a3e3b49733793fa5154c1caef13
聂腾飞 <812071523@qq.com>
2020-08-26 21:23:01 +0800

    add: 初步新增input组件

---
+++ src/input.vue
@@ -0,0 +1,24 @@
+<template>
+   <div></div>
+</template>
+
+<script>
+export default {
+   data () {
+      return {
+
+      };
+   },
+
+   components: {},
+
+   watch: {},
+
+   computed: {},
+
+   methods: {}
+}
+</script>
+<style lang='less' scoped>
+
+</style>
