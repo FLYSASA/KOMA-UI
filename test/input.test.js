08c77b5c2f43f49232be7f75d73ca2ec0794aa4b
FLYSASA <812071523@qq.com>
2020-08-29 17:50:07 +0800

    fix(location): 解决travis-ci报引入错误问题

--- test/input.test.js
+++ test/input.test.js
@@ -1,6 +1,6 @@
 const expect = chai.expect;
 import Vue from 'vue'
-import Input from '../src/Input'
+import Input from '../src/input'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
