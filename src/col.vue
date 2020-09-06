be2bbae5a42647cd36f40c93badb3b15628e1ff7
FLYSASA <812071523@qq.com>
2020-09-06 09:27:51 +0800

    add: 完成row,col的雏形

---
+++ src/col.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="col" :class="[ span && `col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'KomaCol',
+  props: {
+    span: {
+      type: [String, Number]
+    }
+  },
+  data () {
+    return {
+    };
+  },
+  computed: {},
+  created () {},
+  methods: {}
+}
+
+</script>
+<style lang='less'>
+.col{
+  height: 100px;
+  background: gray;
+  width: 50%;
+  border: 1px solid green;
+  // &.col-1{
+  //   width: 1/24%;
+  // }
+  @class: col-;
+  .col-loop(@n) when (@n>0){
+    &.@{class}@{n}{
+        width: @n/24*100%;
+    }
+    .col-loop((@n)-1);
+  }
+  .col-loop(24)
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    };
+  },
+  computed: {},
+  created () {},
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+    }
+</style>
