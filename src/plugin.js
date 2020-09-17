8887c401eee4f7c42ed2996e80e3034b577ad80f
FLYSASA <812071523@qq.com>
2020-09-18 00:04:32 +0800

    add: 支持toast enableHtml 多行文本功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,11 @@
 import Toast from './toast'
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = function(message){
+    Vue.prototype.$toast = function(message, toastOptions){
       const Constructor = Vue.extend(Toast)
       const toast = new Constructor({
         propsData: {
-          closeButton: {
-            text: '知道了',
-            callback() {
-              console.log('偶也')
-            }
-          }
+          ...toastOptions
         }
       })
       toast.$slots.default = message;
--- src/toast.vue
+++ src/toast.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="toast">
-    <slot></slot>
-    <div class="line">
+  <div class="toast" ref="toast">
+    <div class="message-wrapper">
+      <slot v-if="!enableHtml"></slot>
+      <div v-else v-html="$slots.default"></div>
     </div>
+    <div class="line" ref="line"></div>
     <span class="close" v-if="closeButton" @click="onclickClose">
       {{ closeButton.text }}
     </span>
@@ -30,6 +32,10 @@ export default {
           text: '关闭'
         }
       }
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -38,18 +44,28 @@ export default {
   },
   computed: {},
   mounted() {
-    if (this.autoClose) {
-      setTimeout(()=>{
-        // this.close();
-      }, this.autoCloseDelay * 1000);
-    }
+    this.execAutoClose()
+    this.updateStyle()
   },
   created () {},
   methods: {
+    updateStyle () {
+      this.$nextTick(()=>{
+        let height = this.$refs['toast'].getBoundingClientRect().height
+        this.$refs['line'].style.height = `${height}px`
+      })
+    },
+    execAutoClose() {
+      if (this.autoClose) {
+        setTimeout(()=>{
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
     onclickClose(){
       this.close()
-      if(this.closeButton.callback && typeof(this.closeButton.callback) === 'function') {
-        this.closeButton.callback()
+      if(this.closeButton.callback && typeof this.closeButton.callback === 'function') {
+        this.closeButton.callback(this)
       }
     },
     close() {
@@ -62,12 +78,12 @@ export default {
 </script>
 <style lang='less' scoped>
 @font-size: 14px;
-@toast-height: 40px;
+@toast-min-height: 40px;
 @toast-bg: rgba(0, 0, 0, 0.75);
 .toast {
   font-size: @font-size;
   line-height: 1.8;
-  height: @toast-height;
+  min-height: @toast-min-height;
   background: @toast-bg;
   color: #fff;
   position: fixed;
@@ -79,13 +95,16 @@ export default {
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   padding: 0 16px;
   border-radius: 4px;
+  .message-wrapper {
+    padding: 8px 0;
+  }
   .close {
+    flex-shrink: 0;
     cursor: pointer;
   }
   .line {
     border-left: 1px solid #666;
     margin: 0 16px;
-    height: 100%;
   }
 }
 </style>
