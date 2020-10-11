7e5ed2e9617db2399186e075904ae1fe06361c6c
FLYSASA <812071523@qq.com>
2020-10-11 21:50:32 +0800

    add:添加popover插槽api

--- src/popover.vue
+++ src/popover.vue
@@ -2,7 +2,7 @@
   <div class="popover" ref="popover">
     <div ref="popoverContent" class="content-wrapper" v-if="visible"
     :class="{[`position-${position}`]:true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
