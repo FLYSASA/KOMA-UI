b911be4c44998e9ed495570dc3b3e77d13981806
聂腾飞 <812071523@qq.com>
2020-10-16 21:33:45 +0800

    add: 初步新增collapse组件

---
+++ src/collapse-item.vue
@@ -0,0 +1,41 @@
+<template>
+  <div  class="collapse-item">
+    <div class="title">
+      {{title}}
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KomaCollapseItem',
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+
+    };
+  },
+
+  computed: {},
+
+  created() {},
+
+  methods: {},
+};
+</script>
+<style lang='less' scoped>
+  .collapse-item {
+    > .title {
+
+    }
+  }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KomaCollapse',
+  components: {},
+  data() {
+    return {
+
+    };
+  },
+
+  computed: {},
+
+  created() {},
+
+  methods: {},
+};
+</script>
+<style lang='less' scoped>
+  @gray: #ddd;
+  .collapse {
+    border: 1px solid @gray;
+  }
+</style>
