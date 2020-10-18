7ef17f0d59b6feab57a89258e47b0e038e8552ef
FLYSASA <812071523@qq.com>
2020-10-18 11:05:50 +0800

    add: 初步新增vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'KOMA-UI1',
+  description: '一个好用的UI框架',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      '/install/',
+      '/get-started/',
+    ]
+  }
+}
