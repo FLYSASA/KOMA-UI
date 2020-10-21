aff805cc234ab745e53d9899848e6794e0342825
FLYSASA <812071523@qq.com>
2020-10-21 21:43:22 +0800

    refactor: 重构button css变量

--- src/button.vue
+++ src/button.vue
@@ -40,19 +40,27 @@
     0% { transform: rotate(0deg);}
     100% { transform: rotate(360deg);}
   }
+  @font-size: 14px;
+  @button-height: 32px;
+  @button-bg: white;
+  @button-active-bg: #eee;
+  @border-radius: 4px;
+  @color: #333;
+  @border-color: #999;
+  @border-color-hover: #666;
   .g-button {
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    font-size: var(--font-size);
-    height: var(--button-height);
+    font-size: @font-size;
+    height: @button-height;
     padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: @border-radius;
+    border: 1px solid @border-color;
+    background: @button-bg;
     vertical-align: middle;
-    &:hover {border-color: var(--border-color-hover);}
-    &:active { background-color: var(--button-active-bg); }
+    &:hover {border-color: @border-color-hover;}
+    &:active { background-color: @button-active-bg; }
     &:focus { outline: none; }
     > .icon { order: 1; margin-right: .3em; }
     > .content{
