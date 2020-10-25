96730aee4fa5872a28bb5bca9854e30f537c74e4
FLYSASA <812071523@qq.com>
2020-10-26 00:42:22 +0800

    module: 打包工具从 parcel 迁移到 vue-cli

--- index.js
+++
@@ -1,5 +0,0 @@
-import Button from './src/button.vue';
-import ButtonGroup from './src/button-group.vue';
-import Icon from './src/icon.vue';
-
-export { Button, ButtonGroup, Icon }
---
+++ src/demo.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <g-cascader :datas="cascaderDatas" popover-height="200px"></g-cascader>
+  </div>
+</template>
+
+<script>
+import Cascader from './cascader'
+export default {
+  name: 'demo',
+  components: {
+    'g-cascader': Cascader
+  },
+  props: {},
+  data () {
+    return {
+      cascaderDatas: [
+      {
+        name: '浙江',
+        key: 'zhejiang',
+        children: [
+          {
+            name: '杭州',
+            key: 'hangzhou',
+            children: [
+              {
+                name: '上城区',
+                key: 'shangchengqu',
+              },
+              {
+                name: '下城区',
+                key: 'xiaChengqu',
+              }
+            ]
+          },
+          {
+            name: '嘉兴',
+            key: 'jiaxing',
+          },
+          {
+            name: '湖州',
+            key: 'huzhou',
+          }
+        ]
+      },
+      {
+        name: '福建',
+        key: 'fujian',
+        children: [
+          {
+            name: '福州',
+            key: 'fuzhou',
+            children: [
+              {
+                name: '鼓楼',
+                key: 'gulou',
+              },
+              {
+                name: '台江',
+                key: 'taijiang',
+              }
+            ]
+          },
+          {
+            name: '莆田',
+            key: 'putian',
+          }
+        ]
+      }
+    ]
+    };
+  },
+  computed: {},
+  created () {},
+  methods: {}
+}
+
+</script>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    --font-size: 14px;
+  }
+  body {
+    font-size: var(--font-size)
+  }
+
+</style>
