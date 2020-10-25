76d9574fa5ca19e4e160c9bb15182127962190fe
FLYSASA <812071523@qq.com>
2020-10-25 10:56:51 +0800

    add: 粗糙实现cascader

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <div class="cascader-wrapper">
+    <div class="cascader">
+      <div class="trigger" @click="popoverVisible = !popoverVisible">
+      </div>
+      <div class="popover" v-if="popoverVisible">
+        <div class="level1">
+          <div class="label" v-for="item1 in datas" @click="selectedItem1 = item1">
+            {{ item1.name }}
+          </div>
+        </div>
+        <div class="level2">
+          <div class="label" v-for="item2 in level2Items" @click="selectedItem2 = item2">
+            {{ item2.name }}
+          </div>
+        </div>
+        <div class="level3">
+          <div class="label" v-for="item3 in level3Items" >
+            {{ item3.name }}
+          </div>
+        </div>
+      </div>
       <slot></slot>
     </div>
-    <div class="popover">
+    <!-- <div class="popover">
       <cascader-item v-for="item in datas" :sourceItem="item"></cascader-item>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -21,10 +40,20 @@ export default {
   },
   data() {
     return {
+      popoverVisible: true,
+      selectedItem1: null,
+      selectedItem2: null,
     };
   },
 
-  computed: {},
+  computed: {
+    level2Items(){
+      return this.selectedItem1 && this.selectedItem1.children || []
+    },
+    level3Items(){
+      return this.selectedItem2 && this.selectedItem2.children || []
+    }
+  },
 
   created() {
     console.log(this.datas)
@@ -35,7 +64,20 @@ export default {
 </script>
 <style lang='less' scoped>
   @import './css/_var';
-  .cascader-wrapper {
-
+  .cascader {
+    .trigger {
+      border: 1px solid red;
+      height: @input-height;
+      width: 100px;
+    }
+    .popover {
+      border: 1px solid red;
+      height: 200px;
+      position: relative;
+      display: flex;
+      .label {
+        white-space: nowrap;
+      }
+    }
   }
 </style>
