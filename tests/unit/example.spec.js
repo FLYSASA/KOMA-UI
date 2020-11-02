d01f311d0db2d90c581c8beb8fc53533bbaf9848
聂腾飞 <812071523@qq.com>
2020-11-02 15:11:24 +0800

    add：新增test-watch指令， 格式化svg.js

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    expect(1).to.eq(1)
+  })
+})
