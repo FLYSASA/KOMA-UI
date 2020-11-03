766aef56edb7d299ab1cb9d55ec9f51638a47a9b
聂腾飞 <812071523@qq.com>
2020-11-03 14:57:37 +0800

    refactor: 重构popover的测试用例，新增popover trigger测试

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -43,28 +43,26 @@ describe('Button', () => {
     expect(getComputedStyle(icon).order).to.eq('1')
   })
   it('设置 iconPosition 可以改变 order', () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
+    const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
         iconPosition: 'right'
       }
-    }).$mount(div)
+    })
+    const vm = wrapper.vm
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('2')
     vm.$el.remove()
     vm.$destroy()
   })
   it('点击 button 触发 click 事件', () => {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
+    const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
       }
-    }).$mount()
-
+    })
+    const vm = wrapper.vm
+    
     const callback = sinon.fake();
     vm.$on('click', callback)
     vm.$el.click()
---
+++ tests/unit/popover.spec.js
@@ -0,0 +1,55 @@
+import chai, { expect } from 'chai';
+import Vue from 'vue';
+import { mount } from '@vue/test-utils'
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+import Popover from '@/popover';
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+  it('存在.', () => {
+    expect(Popover).to.exist
+  })
+  it('可以设置 position', () => {
+    const wrapper = mount(Popover, {
+      slots: {
+        default: {
+          template: `<button>点我</button>`,
+        },
+        content: '<div>弹出内容</div>'
+      },
+      propsData: {
+        position: 'bottom'
+      }
+    })
+    const vm = wrapper.vm
+    wrapper.find('button').trigger('click');
+    vm.$nextTick().then(()=>{
+      const classes = wrapper.find('.content-wrapper').classes()
+      expect(classes).to.include('position-bottom')
+    })
+  })
+  it('可以设置 trigger', () => {
+    const wrapper = mount(Popover, {
+      slots: {
+        default: {
+          template: `<button>点我</button>`,
+        },
+        content: '<div>弹出内容</div>'
+      },
+      propsData: {
+        position: 'bottom',
+        trigger: 'hover'
+      }
+    })
+    const vm = wrapper.vm
+    expect(wrapper.find('.content-wrapper').element).to.not.exist
+    wrapper.find('.popover').trigger('mouseenter');
+    vm.$nextTick().then(()=>{
+      expect(wrapper.find('.content-wrapper').element).to.exist
+    })
+  })
+})
