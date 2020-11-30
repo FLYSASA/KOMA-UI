943dab11cc92eaab866b42f223f78d3c18b4563b
FLYSASA <812071523@qq.com>
2020-11-30 23:29:12 +0800

    add: 初步添加pager组件

---
+++ src/components/pager/pager.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="koma-pager">
+    <span v-for="page in pages">
+      {{page}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KomaPager',
+  components: {},
+  props: {
+    totalPage: {
+      type: Number,
+      required: true
+    },
+    currentPage: {
+      type: Number,
+      required:true
+    },
+    hideIfOnePage: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    let pages = []
+    for(let i = 1; i <= this.totalPage; i++) {
+      pages.push(i)
+    }
+    return {
+      pages
+    };
+  },
+  computed: {},
+  created () {},
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
--- src/demo.vue
+++ src/demo.vue
@@ -1,64 +1,23 @@
 <template>
   <div id="app">
-    <div>
-      <div class="form-wrapper">
-        <g-form class="form" :model="model" :rules="rules" ref="form">
-          <h1>登录</h1>
-          <g-form-item label="邮箱" prop="email">
-            <g-input type="text" v-model="model.email"></g-input>
-          </g-form-item>
-          <g-form-item label="密码" prop="password">
-            <g-input type="password" v-model="model.password"></g-input>
-          </g-form-item>
-          <div>
-            <g-button class="ok" type="submit" @click="onSubmit">提交</g-button>
-          </div>
-        </g-form>
-      </div>
-    </div>
+    <g-pager total-page="10" current-page="5"></g-pager>
   </div>
 </template>
 
 <script>
-import GButton from '@/components/button/button';
-import GInput from '@/components/input';
-import GForm from '@/components/form/form';
-import GFormItem from '@/components/form/form-item';
-import formMixin from '@/components/form/form-mixin';
+import GPager from '@/components/pager/pager';
 
 export default {
   name: 'ButtonDemo',
   components: {
-    GButton,
-    GInput,
-    GForm,
-    GFormItem
+    GPager
   },
-  mixins: [formMixin],
   props: {},
   data () {
     return {
-      model: {
-        email: '',
-        password: ''
-      },
-      rules: [
-        { key: 'email', pattern: 'email', required: true},
-        { key: 'password', minLength: 6, required: true}
-      ]
     };
   },
-  watch: {},
-  computed: {},
-  methods: {
-    onSubmit () {
-      this.$refs['form'].validate((valid)=>{
-        if(valid){
-          console.log(valid)
-        }
-      })
-    }
-  },
+  methods: {},
 }
 
 </script>
@@ -67,9 +26,6 @@ export default {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-  .form-wrapper{
-    padding: 20px;
   }
 
 </style>
