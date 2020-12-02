01c74a3be3497a2368e9fac7571ae6ffc0684c53
聂腾飞 <812071523@qq.com>
2020-12-02 14:26:29 +0800

    add(docs): 添加pager demo

---
+++ docs/.vuepress/components/pager-demos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <g-pager :total-page="20" :current-page.sync="currentPage"></g-pager>
+  </div>
+</template>
+
+<script>
+import GPager from '@/components/pager/pager';
+
+export default {
+  name: 'ButtonDemo',
+  components: {
+    GPager
+  },
+  props: {},
+  data () {
+    return {
+      currentPage: 1
+    };
+  },
+  methods: {},
+}
+
+</script>
+<style lang="less">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    margin: 20px;
+  }
+
+</style>
