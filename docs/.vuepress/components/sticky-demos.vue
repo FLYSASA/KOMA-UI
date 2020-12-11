4b4d362c5483536d34033787ce283f58d6deb418
聂腾飞 <812071523@qq.com>
2020-12-11 15:15:07 +0800

    add: 使用vue-cli lib打包模式

---
+++ docs/.vuepress/components/sticky-demos.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app" style="display: flex; justify-content: center;">
+    <div style="width: 600px; border: 1px solid red;">
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+      <g-sticky :distance="100">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GhcDzDyr_yvRAr4rkSR2p3M5NedTq4gLjU42GqB61FbccsfnSPoIT5VxYg&usqp=CAc" alt="">
+      </g-sticky>
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+      <p>段落1</p>
+      <p>段落2</p>
+      <p>段落3</p>
+      <p>段落4</p>
+      <p>段落5</p>
+      <p>段落6</p>
+      <p>段落7</p>
+      <p>段落8</p>
+      <p>段落9</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import GSticky from '@/components/sticky';
+
+export default {
+  name: 'demo',
+  components: {
+    GSticky
+  },
+  props: {},
+  data () {
+    return {};
+  },
+  watch: {},
+  methods: {},
+}
+
+</script>
+<style lang="less">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  ul, li {
+    list-style: none;
+  }
+  .box{
+    border: 1px solid red;
+  }
+
+</style>
--- src/demo.vue
+++ src/demo.vue
@@ -1,63 +1,9 @@
 <template>
   <div id="app" style="display: flex; justify-content: center;">
     <div style="width: 600px; border: 1px solid red;">
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
       <g-sticky :distance="100">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GhcDzDyr_yvRAr4rkSR2p3M5NedTq4gLjU42GqB61FbccsfnSPoIT5VxYg&usqp=CAc" alt="">
       </g-sticky>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
     </div>
   </div>
 </template>
